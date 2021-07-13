65faa8696707b03cdc435fca55794c721ea0838a
laxmi821 <84071540+laxmi821@users.noreply.github.com>
2021-07-13 16:05:29 +0530

    Update main.py

--- main.py
+++ main.py
@@ -3,15 +3,15 @@ from pytube import YouTube
 url = ('Enter you youtube url here')
 my_video = YouTube(url)
 
-print("*********************Video Title************************")
+print("~*~*~*~*~Video Title~*~*~*~*~*")
 #get Video Title
 print(my_video.title)
 
-print("********************Tumbnail Image***********************")
+print("~*~*~*~*Tumbnail Image*~*~*~*~*")
 #get Thumbnail Image
 print(my_video.thumbnail_url)
 
-print("********************Download video*************************")
+print("*~*~*~*~*Download video*~*~*~*~*~*")
 #get all the stream resolution for the
 for stream in my_video.streams:
     print(stream)
